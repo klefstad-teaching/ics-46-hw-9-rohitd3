a8d9e6d4faf56b7a100214572807ffdb6fdc72ea
Rohit De <de.rohit01@gmail.com>
2025-03-16 22:34:36 +0000

    working on dijkstras

---
+++ src/dijkstras.cpp
@@ -0,0 +1,53 @@
+#include "dijkstras.h"
+
+
+using namespace std;
+
+vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination) {
+    vector<int> path;
+    int current = destination;
+    while (current != -1) {
+        path.push_back(current);
+        current = previous[current];
+    }
+    reverse(path.begin(), path.end());
+    return path;
+}
+
+void print_path(const vector<int>& v, int total) {
+    cout << "Path: ";
+    for (size_t i = 0; i < v.size(); ++i) {
+        cout << v[i];
+        if (i < v.size() - 1) {
+            cout << " → ";
+        }
+    }
+    cout << ", Total Cost: " << total << endl;
+}
+
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
+    int n = G.size();
+    vector<int> distance(n, INF);
+    distance[source] = 0;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, source});
+
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+
+        for (const Edge& e : G[u]) {
+            int v = e.dst;
+            int weight = e.weight;
+
+            if (distance[u] + weight < distance[v]) {
+                distance[v] = distance[u] + weight;
+                previous[v] = u;
+                pq.push({distance[v], v});
+            }
+        }
+    }
+
+    return distance;
+}
