09f08955faffa7607bad1e85d33ef3bb137cb204
Rohit De <de.rohit01@gmail.com>
2025-03-16 22:47:17 +0000

    endl?

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -28,10 +28,9 @@ void print_path(const vector<int>& path, int total_cost) {
     for (size_t i = 0; i < path.size(); ++i) {
         cout << path[i];
         if (i < path.size() - 1) {
-            cout << "  ";
+            cout << " ";
         }
     }
-    cout << endl;
     cout << "Total cost is " << total_cost << endl;
 }
 
