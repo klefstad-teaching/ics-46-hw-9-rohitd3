302485f9c0808f83caf01a8d918f7c6f6563962e
Rohit De <de.rohit01@gmail.com>
2025-03-17 01:10:45 +0000

    ladder main test

---
+++ src/ladder_main.cpp
@@ -0,0 +1,20 @@
+#include "ladder.h"
+#include <string>
+
+int main() {
+    set<string> word_list;
+    load_words(word_list, "words.txt");
+
+    string start_word, end_word;
+    cout << "Enter start word: ";
+    cin >> start_word;
+    cout << "Enter end word: ";
+    cin >> end_word;
+
+    vector<string> ladder = generate_word_ladder(start_word, end_word, word_list);
+    print_word_ladder(ladder);
+
+    // verify_word_ladder();
+
+    return 0;
+}
